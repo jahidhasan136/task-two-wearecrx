b71bc6605b9e5ed47b154b50d95e9c3d24b9cb58
jahidhasan136 <jh3495689@gmail.com>
2025-09-30 01:28:20 +0600

    page setup

---
+++ src/app/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <>
+      <p>Phone: phone</p>
+      <p>Address: address</p>
+    </>
+  );
+};
+
+export default Contact;
---
+++ src/app/components/Heading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <>
+      <h1>[[ Quick | Fast | Speedy ]] delivery service in dhaka.</h1>;
+    </>
+  );
+};
+
+export default Heading;
